1d5ad92f29f339b0542dd10a4ec3331a8cd8b576
Gawain Hammond <gawain@touchy.local>
2017-10-06 16:52:04 +0100

    attempt to get marketplace test working

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -14,9 +14,8 @@ contract Marketplace {
 
     struct Auction {
            // Location and ownership information of the item for sale
+          address seller;
            uint auctionId;
-           address seller;
-           //address contractAddress; // Contract where the item exists
 
            // Auction metadata
            uint units;
@@ -24,14 +23,16 @@ contract Marketplace {
            bool isLive;
            uint winningBid;
 
-           Bid bid;
+           //Bid bid;
        }
 
 
-    Auction[] private auctions;
+     Auction[] private auctions;
 
     //TODO reminder to research and implements events for auditing...
     //event AuctionCreated(uint id, string title, uint256 startingPrice, uint256 reservePrice);
+
+    event AuctionCreated(address seller,  uint auctionId, uint totals);
 
     function Marketplace() {
         //nuttin' to see here yet, move on - @domfox the Catford kingpin
@@ -39,11 +40,15 @@ contract Marketplace {
 
     function createAuction(uint _units, uint256 _price) returns (uint auctionId) {
 
-        auctionId = auctions.length++;
-        Auction a = auctions[auctionId];
-        a.seller = msg.sender;
-        a.unitPrice = _price;
-        a.units = _units;
+        auctionId = auctions.length + 1;
+        auctions.push(Auction({auctionId: auctionId,
+                                seller: msg.sender,
+                                unitPrice: _price,
+                                units: _units,
+                                isLive: true,
+                                winningBid: 0}));
+
+        AuctionCreated(msg.sender, auctionId, auctions.length);
 
         // create event, see above
         // AuctionCreated(auctionId, a.title, a.startingPrice, a.reservePrice);
@@ -56,7 +61,7 @@ contract Marketplace {
 
     // for now units must match units advertised
        //uint256 amount = msg.value; // example of current transfer?
-        Auction a = auctions[auctionId];
+        Auction storage a = auctions[auctionId];
 
        // do checks, throw toys out of pram / shit pants (delete as appropriate)
        if (a.unitPrice > _unitPrice) return false; // or throw?
@@ -64,15 +69,39 @@ contract Marketplace {
 
        // end auction
        a.isLive= false;
-       a.bid = Bid(msg.sender, _unitPrice, now);
+       //a.bid = Bid(msg.sender, _unitPrice, now);
 
        return true;
    }
 
 
-   function getAuctions() constant returns (bytes32[]) {
-      bytes32[] _auctions;
-       return _auctions;
-   }
+   function getAuctions() constant returns (uint[]) {
+      uint[] memory _auctions;
+       for(uint i = 0; i < auctions.length; i++) {
+            Auction storage a = auctions[i];
+            _auctions[i] = a.auctionId;
+        }
+      return _auctions;
+    }
+
+
+    function numAuctions() constant returns (uint) {
+       return auctions.length;
+     }
+
+
+    function uintToBytes(uint v) constant returns (bytes32 ret) {
+        if (v == 0) {
+            ret = '0';
+        }
+        else {
+            while (v > 0) {
+                ret = bytes32(uint(ret) / (2 ** 8));
+                ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+                v /= 10;
+            }
+        }
+        return ret;
+    }
 
 }

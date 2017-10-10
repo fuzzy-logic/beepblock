d70607f10cdbba99e184aee41fcdea5d95fcdf4e
Gawain Hammond <gawain@touchy.local>
2017-10-10 18:22:11 +0100

    added find energy function to market place

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -6,27 +6,62 @@ pragma solidity ^0.4.13;
 contract Marketplace {
 
     struct Auction {
+          bytes32 id;
           address seller;
-           uint units;
            uint unitPrice;
        }
 
 
-    Auction[] auctions;
+    bytes32[] auctionIndex;
+    mapping(bytes32 => Auction) public auctions;
 
 
     function Marketplace() {
         //nuttin' to see here yet, move on - @domfox the Catford kingpin
     }
 
-    function createAuction(uint _units, uint _price) returns (bool) {
-        Auction memory auction = Auction({seller: msg.sender, units: _units, unitPrice: _price});
-        auctions.push(auction);
+    function createAuction(uint _price) returns (bool) {
+        bytes32 id = keccak256(msg.sender, _price);
+        Auction memory auction = Auction({id: id, seller: msg.sender, unitPrice: _price});
+        auctions[id] = auction;
+        auctionIndex.push(id);
         return true;
     }
 
     function auctionCount() constant returns (uint) {
-        return auctions.length;
+        return auctionIndex.length;
     }
+
+    function index() constant returns (bytes32[]) {
+        return auctionIndex;
+    }
+
+    function getAuctionById(bytes32 auctionId) constant returns (bytes32 id, address seller, uint unitPrice) {
+        Auction a = auctions[id];
+        uint price = 90;
+        return (a.id, a.seller, a.unitPrice);
+    }
+
+    function findCheapestAuction(uint _maxPrice) constant returns (bytes32 cheapestId) {
+      bytes32 NONE = bytes32(0);
+      uint  MAX_PRICE = 0xFFFFFFFF;
+      uint  cheapestPrice = MAX_PRICE; //intentionally maxed out so any price is lower;
+      for(uint i = 0; i < auctionIndex.length; i++)   {
+          Auction memory auction = auctions[auctionIndex[i]];
+          if (auction.unitPrice < cheapestPrice) {
+            cheapestPrice = auction.unitPrice;
+            cheapestId = auction.id;
+          }
+      }
+      if (cheapestId != NONE && cheapestPrice < _maxPrice) {
+        return cheapestId;
+      } else {
+        return 0;
+      }
+    }
+
+
+
+
 
 }
--- test/MarketplaceTest.js
+++ test/MarketplaceTest.js
@@ -14,17 +14,37 @@ var Marketplace = artifacts.require("./Marketplace.sol");
       }).then(function(auctions) {
         console.log('initial num auctions: ' + auctions);
         assert.equal(auctions, 0, "a newly created marketplace should have zero auctions");
-        return marketplaceInstance.createAuction(10, 120);
+        return marketplaceInstance.createAuction(120);
       }).then(function(tx) {
         console.log('tx: ' + JSON.stringify(tx));
-        return marketplaceInstance.createAuction(20, 110);
+        return marketplaceInstance.createAuction(110);
       }).then(function(tx) {
         console.log('tx: ' + JSON.stringify(tx));
-        return marketplaceInstance.auctionCount();
-      }).then(function(auctions) {
-        console.log('num auctions: ' + JSON.stringify(auctions));
-        console.dir(auctions);
-        assert.equal(auctions, 2, "there should be 2 auctions in total");
+        return marketplaceInstance.createAuction(100);
+      }).then(function(tx) {
+        console.log('tx: ' + JSON.stringify(tx));
+        return marketplaceInstance.createAuction(90);
+      }).then(function(tx) {
+        console.log('tx: ' + JSON.stringify(tx));
+        return marketplaceInstance.index();
+      }).then(function(index) {
+        console.log('index: ' + JSON.stringify(index));
+        //console.dir(index);
+        assert.equal(index.length, 4, "index size expcted to be 4");
+        console.log('finding cheapest auction....');
+        return marketplaceInstance.findCheapestAuction(105);
+      }).then(function(cheapestAuctionId) {
+        console.log('cheapestAuctionId: ' + JSON.stringify(cheapestAuctionId));
+        assert.ok(cheapestAuctionId, "cheapest auction expected to be not null");
+        return  marketplaceInstance.getAuctionById(cheapestAuctionId);
+      }).then(function(auction) {
+        console.log('auction: ' + JSON.stringify(auction));
+        var id = auction[0];
+        var sellerAddress = auction[1];
+        var price = auction[2];
+        assert.ok(id, "retrieved auction id expected to be not null");
+        assert.ok(sellerAddress, "retrieved auction id expected to be not null");
+        assert.equal(price, 90, "retrieved cheapest auction price expected to be 90");
       });
     });
   });
